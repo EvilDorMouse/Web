2277a3884d3ba487acac460c2758d4cb2fb51ef7
EvilDorMouse <vasilchenko.da@mail.ru>
2023-09-13 17:36:12 +0700

    summary

---
+++ main_Page/src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<div class="frame">
+    <div class="phone">
+        <div class="bgRectangle"></div>
+        <svg width="394" height= "800">
+            <circle cx="164.835" cy="189.145" r="145" stroke="#F6F6F6" stroke-width="0.12" fill="none"/>
+            <rect x="56" y="-1" width="334" height="589" style="fill:none;stroke-width:0.13;stroke:#F6F6F6"/>
+            <polygon points="-75.4926,55.66858 591.736,183.932 162.746,710.821" style="fill:none;stroke:#F6F6F6;stroke-width:0.11"/>
+        </svg>
+        
+        <header>
+            <nav>
+                <a href="#" class="logo"><img src="/Logo.svg" alt=""></a>
+                <a href="#" class="menu">menu</a>
+            </nav>
+        </header>
+        <main>
+            <h1>Our “go digital” methodology:</h1>
+            <p>
+                We need to come up with a few 
+                lines about how we do about 
+                actioning a digital transformation 
+                case.<br>
+                Steps we follow and why they are 
+                important.<br>
+                Our digital transformation - 
+                business process, a path that we 
+                would take our clients on.<br>
+                That will also assist us in writing 
+                client business documentation = 
+                very important part.
+            </p>
+        </main>
+        <footer>
+            <p>
+                check out our<br>
+                projects<br>
+                9 ... and counting
+            </p>
+            <button type="button"><img src="/Arrow.svg" alt=""></button>
+        </footer>
+    </div>
+</div>
+
+
+
+
+<style>
+    @import '../lib/style/app.scss';
+</style>
